dbd3347f9e990394d933976be52bf62b89c3b036
Armando Plácido <armando.junior4440@gmail.com>
2024-03-27 21:56:36 -0300

    create validation transaction method

--- src/main/java/br/com/armandoplacido/picpaydesafiobackend/transaction/TransactionService.java
+++ src/main/java/br/com/armandoplacido/picpaydesafiobackend/transaction/TransactionService.java
@@ -1,6 +1,9 @@
 package br.com.armandoplacido.picpaydesafiobackend.transaction;
 
+import br.com.armandoplacido.picpaydesafiobackend.transaction.exception.InvalidTransactionException;
+import br.com.armandoplacido.picpaydesafiobackend.wallet.Wallet;
 import br.com.armandoplacido.picpaydesafiobackend.wallet.WalletRepository;
+import br.com.armandoplacido.picpaydesafiobackend.wallet.WalletType;
 
 public class TransactionService {
   private final TransactionRepository transactionRepository;
@@ -12,5 +15,23 @@ public class TransactionService {
     ) {
     this.transactionRepository = transactionRepository;
     this.walletRepository = walletRepository;
+  }
+
+  public void createTransaction(Transaction transaction){
+
+  }
+
+  private void validate(Transaction transaction) {
+    walletRepository.findById(transaction.payee())
+    .map(payee -> walletRepository.findById(transaction.payer())
+      .map(payer -> isValidTransaction(transaction, payer) ? transaction : null)
+      .orElseThrow(() -> new InvalidTransactionException(transaction)))
+    .orElseThrow(() -> new InvalidTransactionException(transaction));
+  }
+
+  private boolean isValidTransaction(Transaction transaction, Wallet payer) {
+    return payer.walletType() == WalletType.COMMON.getValue() && 
+      payer.balance().compareTo(transaction.value()) >= 0 &&
+      !payer.id().equals(transaction.payee());
   }
 }
