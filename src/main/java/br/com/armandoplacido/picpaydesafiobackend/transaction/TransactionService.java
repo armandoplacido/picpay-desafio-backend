694ba299c38fb84cce0ee3644d8e658931c49e02
Armando Plácido <armando.junior4440@gmail.com>
2024-03-27 22:16:33 -0300

    create createTransaction method

--- src/main/java/br/com/armandoplacido/picpaydesafiobackend/transaction/TransactionService.java
+++ src/main/java/br/com/armandoplacido/picpaydesafiobackend/transaction/TransactionService.java
@@ -1,5 +1,7 @@
 package br.com.armandoplacido.picpaydesafiobackend.transaction;
 
+import br.com.armandoplacido.picpaydesafiobackend.authorization.AuthorizationService;
+import br.com.armandoplacido.picpaydesafiobackend.notification.NotificationService;
 import br.com.armandoplacido.picpaydesafiobackend.transaction.exception.InvalidTransactionException;
 import br.com.armandoplacido.picpaydesafiobackend.wallet.Wallet;
 import br.com.armandoplacido.picpaydesafiobackend.wallet.WalletRepository;
@@ -8,17 +10,37 @@ import br.com.armandoplacido.picpaydesafiobackend.wallet.WalletType;
 public class TransactionService {
   private final TransactionRepository transactionRepository;
   private final WalletRepository walletRepository;
+  private final AuthorizationService authorizationService;
+  private final NotificationService notificationService;
+
+
 
   public TransactionService(
     TransactionRepository transactionRepository, 
-    WalletRepository walletRepository
+    WalletRepository walletRepository,
+    AuthorizationService authorizationService, 
+    NotificationService notificationService
     ) {
     this.transactionRepository = transactionRepository;
     this.walletRepository = walletRepository;
+    this.authorizationService = authorizationService;
+    this.notificationService = notificationService;
   }
 
-  public void createTransaction(Transaction transaction){
+  public Transaction createTransaction(Transaction transaction){
     validate(transaction);
+
+    var newTransaction = transactionRepository.save(transaction);
+    var payerWallet = walletRepository.findById(transaction.payer()).get();
+    var payeeWallet = walletRepository.findById(transaction.payee()).get();
+    
+    walletRepository.save(payerWallet.pay(transaction.value()));
+    walletRepository.save(payeeWallet.recieve(transaction.value()));
+    
+    authorizationService.authorizeTransaction(transaction);
+    notificationService.sendNotification(transaction);
+
+    return newTransaction;
   }
 
   private void validate(Transaction transaction) {
